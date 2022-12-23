22e18cc0aa309b7a57aeefc6d0ccf4c7a9b8a7af
HvrdWell <geka23111@gmail.com>
2022-12-24 01:00:09 +0300

    last commit with bug fixxx

--- SwiftulMapApp/DataServices/LocationsDataService.swift
+++ SwiftulMapApp/DataServices/LocationsDataService.swift
@@ -34,6 +34,25 @@ class LocationsDataService {
             ],
             link: "https://en.wikipedia.org/wiki/Pantheon,_Rome"),
         Location(
+            name: "Red Square",
+            cityName: "Moscow",
+            coordinates: CLLocationCoordinate2D(latitude: 55.753544, longitude: 37.621202),
+            description: "Red Square is one of the oldest and largest squares in Moscow, the capital of Russia. Owing to its historical significance and the adjacent historical buildings, it is regarded as one of the most famous squares in Europe and the world.",
+            imageNames: [
+                "moscow-redsquare1",
+                "moscow-redsquare2"
+            ],
+            link: "https://en.wikipedia.org/wiki/Red_Square"),
+        Location(
+            name: "PavukVasya",
+            cityName: "Moscow",
+            coordinates: CLLocationCoordinate2D(latitude: 55.861141, longitude: 37.590973),
+            description: "The House of PavukVasya",
+            imageNames: [
+                "moscow-pavukhome1",
+            ],
+            link: "https://en.wikipedia.org/wiki/PavukVasya"),
+        Location(
             name: "Trevi Fountain",
             cityName: "Rome",
             coordinates: CLLocationCoordinate2D(latitude: 41.9009, longitude: 12.4833),
